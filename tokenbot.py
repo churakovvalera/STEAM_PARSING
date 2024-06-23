9beba02c60686570f17318ee00b015b869de2ff6
Валерий <110198507+churakovvalera@users.noreply.github.com>
2024-06-23 08:37:48 +0300

    Update tokenbot

--- tokenbot.py
+++ tokenbot.py
@@ -1,6 +1,6 @@
 def get_token(x):
     if x==1:
-        return 'MTI0MjcyNDUyNTg4NDMwOTU3NQ.Gl5kgd.iuK4oFCe5eV9ZZxkPEloK3et_UI7uhfljV7_ZY'
+        return 'YOUR TOKEN'
     else:
         exit(-1)
 
